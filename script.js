4322034bbbb4ecd9666fad88bdf779e270458cac
Breon Curry <breoncurry@Breons-MacBook-Pro.local>
2023-10-11 00:21:04 +0900

    Restyled and added Japanese version

--- script.js
+++ script.js
@@ -17,17 +17,3 @@ function dropMenu() {
       }
     }
   }
-
-//Light mode toggle
-const toggle = document.getElementById('toggle');
-const body = document.body;
-
-toggle.addEventListener('input', e => {
-    const isChecked = e.target.checked;
-
-    if (isChecked) {
-        body.classList.add('light-theme');
-    } else {
-        body.classList.remove('light-theme');
-    }
-});
